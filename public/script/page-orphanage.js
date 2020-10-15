ff835729d17970b20b011536eecf8e41ae52c471
Yves de Sá <yvesdsb98@gmail.com>
2020-10-15 16:43:49 -0300

    Creating paga-orphanage and updating page-orphanages

--- public/script/page-orphanage.js
+++ public/script/page-orphanage.js
@@ -1,5 +1,13 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
 // create map
-const map = L.map('mapid').setView([-21.7593034,-41.3247732], 14);
+const map = L.map('mapid',options).setView([-21.7593034,-41.3247732], 14);
 
 // create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,18 +19,29 @@ const icon = L.icon({
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
-})
-
-// create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="public/img/arrow-white.svg"> </a>');
+});
 
 // create and add marker
 L.marker([-21.7593034,-41.3247732],{ icon })
-    .addTo(map)
-    .bindPopup(popup)
- 
+    .addTo(map);
+
+// image gallery
+function selectImage(event){
+    const button = event.currentTarget
+
+    // remove all active classes
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach((button) => {
+        button.classList.remove("active")
+    })
+
+    // select clicked image
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // update the image container
+    imageContainer.src = image.src 
+
+    // add the active class to that button
+    button.classList.add("active")
+}
