06af021e5ca5747a6cf654fb95825787afeae1c6
Yves de Sá <yvesdsb98@gmail.com>
2021-05-30 16:05:39 -0300

    finished frontend

--- public/script/page-create-orphanage.js
+++ public/script/page-create-orphanage.js
@@ -9,10 +9,68 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const icon = L.icon({
     iconUrl: "public/img/map-marker.svg",
     iconSize: [58, 68],
-    iconAnchor: [29, 68],
+    iconAnchor: [29, 68]
 })
 
 // create and add marker
-L.marker([-21.7593034,-41.3247732],{ icon })
+
+let marker;
+
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    // Remove icon
+    marker && map.removeLayer(marker);
+
+    marker = L.marker([lat, lng], { icon })
     .addTo(map)
-    .bindPopup(popup)
+})
+
+// photos upload
+
+function addPhotoField(){
+    const container = document.querySelector('#images')
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    const input = newFieldContainer.children[0]
+    
+    if(input.value != ""){
+        input.value = ""
+        container.appendChild(newFieldContainer)
+    }
+
+}
+
+function removePhotoField(event){
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length <= 1){
+        span.parentNode.children[0].value = ''
+        return
+    }
+
+    span.parentNode.remove()
+}
+
+// select yes or not
+
+function toggleSelect(event) {
+
+    document.querySelectorAll('.button-select button')
+    .forEach(button => button.classList.remove('active'))
+
+    const button = event.currentTarget
+    button.classList.add('active')
+    
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    input.value = button.dataset.value
+}
