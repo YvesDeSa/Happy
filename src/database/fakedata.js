acb4dbf2ee0d200bae533dc56fd92d4bccad63fd
Yves de Sá <yvesdsb98@gmail.com>
2021-06-04 23:54:01 -0300

    Ending the four day of NLW.

---
+++ src/database/fakedata.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-21.7593034",
+        lng: "-41.3247732",
+        name: "Lar das Meninas",
+        description: "",
+        images: [
+
+        ],
+        instructions: "" ,
+        opening_hours: "",
+        open_on_weekends: "1"
+    },
+       {
+        id: 2,
+        lat: "-21.7593034",
+        lng: "-41.3357732",
+        name: "Lar dos meninos",
+        description: "",
+        images: [
+
+        ],
+        instructions: "" ,
+        opening_hours: "",
+        open_on_weekends: "0"
+    }
+]
